5224d46e70797659b3ab1bb04163c0e3865ce4c8
Liveron <minigalimov.lenar@yandex.ru>
2024-01-11 21:46:31 +0300

    Reducer practice

---
+++ react-quiz/src/DateCounter.tsx
@@ -0,0 +1,113 @@
+import { ChangeEvent, useReducer } from "react";
+import ActionBase from "./actions/ActionBase";
+
+enum CounterActionTypes {
+  Inc,
+  Dec,
+  SetCount,
+  SetStep,
+  Reset,
+}
+
+class CounterAction extends ActionBase<CounterActionTypes, number> {
+  constructor(type: CounterActionTypes, payload = 0) {
+    super(type, payload);
+  }
+
+  public get type(): CounterActionTypes {
+    return this._type;
+  }
+  public get payload(): number {
+    return this._payload;
+  }
+}
+
+class CounterState {
+  count = 0;
+  step = 1;
+}
+
+const initialState: CounterState = { count: 0, step: 1 };
+
+function reducer(
+  state: CounterState,
+  action: ActionBase<CounterActionTypes, number>
+): CounterState {
+  switch (action.type) {
+    case CounterActionTypes.Dec:
+      return {
+        ...state,
+        count: state.count >= state.step ? state.count - state.step : 0,
+      };
+    case CounterActionTypes.Inc:
+      return { ...state, count: state.count + state.step };
+    case CounterActionTypes.SetCount:
+      return { ...state, count: action.payload };
+    case CounterActionTypes.SetStep:
+      return { ...state, step: action.payload };
+    case CounterActionTypes.Reset:
+      return initialState;
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+function DateCounter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
+
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  const dec = function () {
+    dispatch(new CounterAction(CounterActionTypes.Dec));
+  };
+
+  const inc = function () {
+    dispatch(new CounterAction(CounterActionTypes.Inc));
+  };
+
+  const defineCount = function (e: ChangeEvent<HTMLInputElement>) {
+    dispatch(
+      new CounterAction(CounterActionTypes.SetCount, Number(e.target.value))
+    );
+  };
+
+  const defineStep = function (e: ChangeEvent<HTMLInputElement>) {
+    dispatch(
+      new CounterAction(CounterActionTypes.SetStep, Number(e.target.value))
+    );
+  };
+
+  const reset = function () {
+    dispatch(new CounterAction(CounterActionTypes.Reset));
+  };
+
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={dec}>-</button>
+        <input value={count} onChange={defineCount} />
+        <button onClick={inc}>+</button>
+      </div>
+
+      <p>{date.toDateString()}</p>
+
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
+export default DateCounter;
---
+++ react-quiz/src/actions/ActionBase.ts
@@ -0,0 +1,11 @@
+export default abstract class ActionBase<T, P> {
+  protected readonly _type: T;
+  protected readonly _payload: P;
+
+  constructor(type: T, payload: P) {
+    this._type = type;
+    this._payload = payload;
+  }
+  public abstract get type(): T;
+  public abstract get payload(): P;
+}
